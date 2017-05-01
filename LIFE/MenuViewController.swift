2ff2abbf41cf07736e1799f2c08267a19cdda931
周靖然 <jrzhou@nat-oitwireless-inside-vapornet100-a-25216.Princeton.EDU>
2017-05-01 18:03:10 -0400

    Add things from LIFE

---
+++ LIFE/MenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  LIFE
+//
+//  Created by Yao Qingning on 10/22/16.
+//  Copyright © 2016 Yao Qingning. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
---
+++ LIFE/MoviesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MoviesViewController.swift
+//  LIFE
+//
+//  Created by Yao Qingning on 10/22/16.
+//  Copyright © 2016 Yao Qingning. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
